3e7093b00656c0090f9f48edd3a7001b7b0e54c6
Cláudio Silva <claudio.silva@impactwave.com>
2016-03-18 18:04:14 +0000

    PhpCode::evalConstant() Minor changes.

--- src/php-code.php
+++ src/php-code.php
@@ -12,7 +12,7 @@ class PhpCode
    *
    * @param callable $wrappedCode  Code to be executed wrapped by error catching code.
    * @param callable $errorHandler Optional error-handling code.
-   * @param bool     $reset        True if the error status should be cleared so that Laravel does not intercept the
+   * @param bool     $reset        True if the error status should be cleared so that later code does not intercept the
    *                               previous error.
    * @return mixed   The return value from the callable argument.
    *
@@ -34,7 +34,8 @@ class PhpCode
       set_error_handler ($prevHandler);
 
       return $r;
-    } catch (Exception $e) {
+    }
+    catch (Exception $e) {
       // Intercept the error that will be triggered below.
       set_error_handler (function () {
         // Force error_get_last() to be set.
@@ -56,6 +57,7 @@ class PhpCode
 
   /**
    * Creates an anonymous function (Closure) that evaluates the given expression.
+   *
    * @param string $_exp  A valid PHP expression. It must not be a statement.
    * @param string $_vars If specified, it's a comma-delimited list of variable names that can be referenced on the
    *                      expression. Names must be prefixed with `$`. Any identifier on the expression that is not on
@@ -69,9 +71,31 @@ class PhpCode
   }
 
   /**
+   * Tries to evaluate a constant value expressed as a string.
+   *
+   * @param string $exp
+   * @param bool   $valid [optional] Outputs `true` if the expression was successfully evaluated.
+   * @return mixed
+   */
+  static function evalConstant ($exp, &$valid = null)
+  {
+    $valid = true;
+    $exp   = trim ($exp);
+    if ($exp[0] == '"' || $exp[0] == "'") return substr ($exp, 1, -1);
+    if (is_numeric ($exp)) {
+      return ctype_digit ($exp) ? intval ($exp) : floatVal ($exp);
+    }
+    if (defined ($exp))
+      return constant ($exp);
+    $valid = false;
+    return null;
+  }
+
+  /**
    * Evaluates the given expression.
    * > **Note:** Variables on the local scope where this method is invoked will not be accessible. Use the `$vars`
    * parameter instead.
+   *
    * @param string $_exp  A valid PHP expression. It must not be a statement.
    * @param array  $_vars If specified, it's a map of variable names and their values that can be referenced on the
    *                      expression. Names must not be prefixed with `$`.
@@ -86,6 +110,7 @@ class PhpCode
   /**
    * Loads and runs a PHP file, searching for it on the include path, stripping the BOM (if one is present) and throwing
    * catchable exceptions instead of fatal errors.
+   *
    * @param string $filename
    * @return mixed|null The value returned from the executed code, or `null` if no `return` is called.
    */
@@ -101,12 +126,14 @@ class PhpCode
   /**
    * Runs PHP code. It supports code either beginning with `<?php` or not but, if `<?php` is present, it must be the
    * first thing on the code string, excluding white space.
+   *
    * @param string $_code The source code.
    * @return mixed
    */
   static function run ($_code)
   {
-    $_code = ltrim ($_code);
+    if (ctype_space ($_code[0]))
+      $_code = ltrim ($_code);
     if (substr ($_code, 0, 5) == '<?php')
       return eval(substr ($_code, 5));
     return eval($_code);
@@ -114,6 +141,7 @@ class PhpCode
 
   /**
    * Checks if the given PHP source code is syntactically valid.
+   *
    * @param string $code
    * @param int    $output If specified, captures the error message.
    * @return bool `true` if the code is syntatically correct.
@@ -139,18 +167,26 @@ class PhpCode
 
   /**
    * Checks if a given PHP expression is syntactically valid.
+   *
    * @param string $exp
    * @return boolean
    */
   static function validateExpression ($exp)
   {
-    return eval ("return true;return $exp;");
+    try {
+      return eval ("return true;return $exp;");
+    }
+      // For PHP7 compat.
+    catch (ParseError $e) {
+      return false;
+    }
   }
 
 }
 
 /**
  * For internal use by {@see PhpCode::compile()}
+ *
  * @private
  * @param string $x
  * @return mixed
