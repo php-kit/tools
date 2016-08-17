1b8446cd72356af33d3bf501d561f5d8215c90ad
Goncalo Martins <goncalo.martins@impactwave.com>
2016-08-17 10:26:03 +0100

    rrmdir changed do work with symlinks on windows

--- src/io.php
+++ src/io.php
@@ -21,19 +21,89 @@ function json_load ($path, $assoc = false)
  */
 function rrmdir ($dir)
 {
-  if (is_dir ($dir)) {
-    $objects = scandir ($dir);
-    foreach ($objects as $object) {
-      if ($object != "." && $object != "..") {
-        if (filetype ($dir . "/" . $object) == "dir")
-          rrmdir ($dir . "/" . $object);
-        else unlink ($dir . "/" . $object);
-      }
+  if (strtoupper (substr (PHP_OS, 0, 3)) === 'WIN') {
+    if (is_symlink ($dir)) {
+      $target = symlink_target ($dir);
+      if (is_dir ($target))
+        rmdir ($dir);
+      else
+        unlink ($dir);
     }
-    reset ($objects);
-    rmdir ($dir);
+    else if (is_dir ($dir)) {
+      foreach (scandir ($dir) as $subdir) {
+        if ($subdir == "." || $subdir == "..")
+          continue;
+        rrmdir ($dir . "/" . $subdir);
+      }
+      rmdir ($dir);
+    }
+    else {
+      unlink ($dir);
+    }
+  }
+  else {
+    if (is_dir ($dir)) {
+      $objects = scandir ($dir);
+      foreach ($objects as $object) {
+        if ($object != "." && $object != "..") {
+          if (filetype ($dir . "/" . $object) == "dir")
+            rrmdir ($dir . "/" . $object);
+          else unlink ($dir . "/" . $object);
+        }
+      }
+      reset ($objects);
+      rmdir ($dir);
+    }
   }
 }
+
+/**
+ * returns if target path is of a symlink
+ *
+ * @param $target
+ * @return bool
+ */
+function is_symlink ($target)
+{
+  $target   = strtr ($target, '\\', '/');
+  $realpath = strtr (realpath ($target), '\\', '/');
+
+  if ($realpath !== $target) // it is a symlink
+    return true;
+
+  // fallback for broken symlinks
+  $link = strtr (readlink ($target), '\\', '/');
+
+  if ($link && $link !== $target)// it is a broken symlink
+    return true;
+
+  return false;
+}
+
+
+/**
+ * returns target of a symlink
+ *
+ * @param $path
+ * @return string
+ */
+function symlink_target ($path)
+{
+  $path   = strtr ($path, '\\', '/');
+  $realpath = strtr (realpath ($path), '\\', '/');
+
+  if ($realpath !== $path) // it is a symlink
+    return $realpath;
+
+  // fallback for broken symlinks
+  $link = strtr (readlink ($path), '\\', '/');
+
+  if ($link && $link !== $path)// it is a broken symlink
+    return $link;
+
+  return $path;
+}
+
 
 define ('DIR_LIST_ALL', 0);
 define ('DIR_LIST_FILES', 1);
@@ -43,6 +113,7 @@ define ('DIR_LIST_DIRECTORIES', 2);
  * List files and/or directories inside the specified path.
  *
  * Note: the `.` and `..` directories are not returned.
+ *
  * @param string   $path
  * @param int      $type      One of the DIR_LIST_XXX constants.
  * @param bool     $fullPaths When `true` returns the full path name of each file, otherwise returns the file name only.
@@ -51,7 +122,7 @@ define ('DIR_LIST_DIRECTORIES', 2);
  */
 function dirList ($path, $type = 0, $fullPaths = false, $sortOrder = false)
 {
-  if (!file_exists($path))
+  if (!file_exists ($path))
     return false;
   $d = new DirectoryIterator($path);
   $o = [];
@@ -91,34 +162,36 @@ function dirList ($path, $type = 0, $fullPaths = false, $sortOrder = false)
  *
  * @copyright Fabien Potencier <fabien@symfony.com>
  */
-function getRelativePath($basePath, $targetPath)
+function getRelativePath ($basePath, $targetPath)
 {
   if ($basePath === $targetPath) {
     return '';
   }
 
-  $sourceDirs = explode('/', isset($basePath[0]) && '/' === $basePath[0] ? substr($basePath, 1) : $basePath);
-  $targetDirs = explode('/', isset($targetPath[0]) && '/' === $targetPath[0] ? substr($targetPath, 1) : $targetPath);
-  array_pop($sourceDirs);
-  $targetFile = array_pop($targetDirs);
+  $sourceDirs = explode ('/', isset($basePath[0]) && '/' === $basePath[0] ? substr ($basePath, 1) : $basePath);
+  $targetDirs = explode ('/', isset($targetPath[0]) && '/' === $targetPath[0] ? substr ($targetPath, 1) : $targetPath);
+  array_pop ($sourceDirs);
+  $targetFile = array_pop ($targetDirs);
 
   foreach ($sourceDirs as $i => $dir) {
     if (isset($targetDirs[$i]) && $dir === $targetDirs[$i]) {
       unset($sourceDirs[$i], $targetDirs[$i]);
-    } else {
+    }
+    else {
       break;
     }
   }
 
   $targetDirs[] = $targetFile;
-  $path = str_repeat('../', count($sourceDirs)).implode('/', $targetDirs);
+  $path         = str_repeat ('../', count ($sourceDirs)) . implode ('/', $targetDirs);
 
   // A reference to the same base directory or an empty subdirectory must be prefixed with "./".
   // This also applies to a segment with a colon character (e.g., "file:colon") that cannot be used
   // as the first segment of a relative-path reference, as it would be mistaken for a scheme name
   // (see http://tools.ietf.org/html/rfc3986#section-4.2).
   return '' === $path || '/' === $path[0]
-         || false !== ($colonPos = strpos($path, ':')) && ($colonPos < ($slashPos = strpos($path, '/')) || false === $slashPos)
+         || false !== ($colonPos = strpos ($path, ':')) &&
+            ($colonPos < ($slashPos = strpos ($path, '/')) || false === $slashPos)
     ? "./$path" : $path;
 }
 
@@ -135,6 +208,7 @@ function fileExists ($filename, $useIncludePath = true)
 
 /**
  * Loads and executes the specified PHP file, searching for it on the 'include path'.
+ *
  * @param string $filename
  * @return bool|mixed
  */
@@ -147,6 +221,7 @@ function includeFile ($filename)
 /**
  * Loads the specified file, optionally searching the include path, and stripping the Unicode Byte Order Mark (BOM), if
  * one is present.
+ *
  * @param string    $filename
  * @param bool|true $useIncludePath
  * @return false|string `false` if the file is not found.
